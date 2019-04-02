0e8fd6a6296f81bf5e3860031bee97bb0e46e5c1
William Ching <willykamus@gmail.com>
2019-04-02 11:02:37 -0400

    First commit, skeleton

---
+++ QuizGame/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by William Ching on 2019-04-02.
+//  Copyright © 2019 William Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
